5103f982a8708347da7dcfd7c44597a29b04ccb9
Slamet Riyadi <slametngeblog@gmail.com>
2019-04-24 20:15:00 +0700

    Initial Commit

---
+++ MasterProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterProject
+//
+//  Created by Qiarra on 24/04/19.
+//  Copyright © 2019 Slamet Riyadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
